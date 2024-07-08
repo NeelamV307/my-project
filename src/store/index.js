f6b00e462233e9a36784dd0117ef967b5715887f
Neelam <neelam@koyaltechsolutions.com>
2024-07-08 13:09:30 +0530

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  },
+  actions: {
+    increment({ commit }) {
+      commit('increment');
+    }
+  },
+  getters: {
+    getCount(state) {
+      return state.count;
+    }
+  }
+});
